50934816343ea4cf9e046dc41573be3d6a24fd33
Nocsis <cchj2638@gmail.com>
2023-02-23 04:04:15 +0800

    add function: tidy spawnpoints data debug: limit the number of spawnpoints to its file lines number

--- spawnpoints-magic-manager.sh
+++ spawnpoints-magic-manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 __version__=0.1.0
-
+is_number='^[0-9]+$'
 Usage() {
     Usage_simple
     echo -e \
@@ -10,6 +10,7 @@ Usage() {
         remove    : remove a spawnpoint (argument: required)
         clear     : clear all spawnpoints (argument: ignored)
         list      : list all spawnpoints (argument: ignored)
+        tidy      : remove duplicate and empty lines (argument: ignored)
         help      : show this help (argument: ignored)
     Arguments:
         <index>   : index of spawnpoint (usually a number)"
@@ -31,8 +32,16 @@ case $1 in
     set)
         if [ -z "$2" ]; then
             target=1
-        else
+        elif ! [[ $2 =~ $is_number ]]; then
             target=$2
+        else
+            echo "Error: $2 is not a number"
+            exit 1
+        fi
+        # check target is le than number of lines
+        if [ $target -gt $(wc -l $SPAWNPOINTS_FILE | cut -d' ' -f1) ]; then
+            echo "Error: $target is greater than number of lines"
+            exit 1
         fi
         echo "Setting spawnpoint to index: $target... $(pwd)"
         sed -i -e $target"i"$(pwd) "$SPAWNPOINTS_FILE"
@@ -55,7 +64,7 @@ case $1 in
         fi
         # TODO: Implement remove by index
         goingtodelete=$(grep -n "$2" $SPAWNPOINTS_FILE | cut -d: -f1)
-        is_number='^[0-9]+$'
+        
         if ! [[ $2 =~ $is_number ]]; then
             echo "Error: $2 is not a number"
             exit 1
@@ -65,6 +74,19 @@ case $1 in
         ;;
     list | ls)
         cat "$SPAWNPOINTS_FILE" | nl
+        ;;
+    tidy)
+        # check file has no empty lines
+        beforelinenum=$(wc -l $SPAWNPOINTS_FILE | cut -d' ' -f1)
+        sed -i '/^\s*$/d' $SPAWNPOINTS_FILE
+        afterlinenum=$(wc -l $SPAWNPOINTS_FILE | cut -d' ' -f1)
+        [ $beforelinenum -eq $afterlinenum ] && echo "No empty lines" || echo "Removed $((beforelinenum - afterlinenum)) empty lines"
+        # check file has no duplicate lines
+        uniq -u $SPAWNPOINTS_FILE > $SPAWNPOINTS_FILE.tmp
+        beforelinenum=$(wc -l $SPAWNPOINTS_FILE | cut -d' ' -f1)
+        afterlinenum=$(wc -l $SPAWNPOINTS_FILE.tmp | cut -d' ' -f1)
+        [ $beforelinenum -eq $afterlinenum ] && echo "No duplicate lines" || echo "Removed $((beforelinenum - afterlinenum)) duplicate lines"
+        mv $SPAWNPOINTS_FILE.tmp $SPAWNPOINTS_FILE
         ;;
     *help | -h)
         Usage
@@ -84,15 +106,3 @@ case $1 in
         exit 1
         ;;
 esac
-
-# check file has no empty lines
-sed -i '/^$/d' $SPAWNPOINTS_FILE
-
-# check file has no duplicate lines
-sort -u $SPAWNPOINTS_FILE -o $SPAWNPOINTS_FILE 
-
-# check character count of file
-if [ $(wc -c < $SPAWNPOINTS_FILE) -eq 0 ]; then
-    echo "File is empty! Creating..."
-    echo $HOME > $SPAWNPOINTS_FILE
-fi
